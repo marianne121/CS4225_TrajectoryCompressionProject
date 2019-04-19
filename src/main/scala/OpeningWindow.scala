91cfbd06b443630d3ae9902cf2639e36395a8fac
unknown <marianne.quek@gmail.com>
2019-04-20 03:37:12 +0800

    working for very small data set, to debug

--- src/main/scala/OpeningWindow.scala
+++ src/main/scala/OpeningWindow.scala
@@ -3,6 +3,8 @@ package sample
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
+import scala.collection.mutable.ArrayBuffer
+
 case class Location(longitude: Double, latitude: Double, timeStamp: Double)
 
 object OpeningWindow {
@@ -18,8 +20,9 @@ object OpeningWindow {
         val raw = rawLocations(linesWithoutHeader)
         //val rawWithIndex = raw.zipWithIndex()
         //val indexKey = rawWithIndex.map{case (k,v) => (v,k)}
-        val distances = calculateDistances(raw)
-        sc.parallelize(distances.collect()).repartition(1).saveAsTextFile(output)
+        val compressed = findCompressed(raw)
+        val compressedRdd = sc.parallelize(compressed)
+        sc.parallelize(compressedRdd.collect()).repartition(1).saveAsTextFile(output)
 
     }
 
@@ -31,20 +34,40 @@ object OpeningWindow {
                 timeStamp = arr(2).toDouble)
     })
 
-    def calculateDistances(locations: RDD[Location]) : RDD[Double] = {
-        val anchor = locations.first()
-        val remaining = locations.mapPartitionsWithIndex{
-            case (index, iterator) => if(index==0) iterator.drop(1) else iterator
-        }
-        val remaining2 = remaining.mapPartitionsWithIndex{
-            case (index, iterator) => if(index==0) iterator.drop(1) else iterator
-        }
-        val floater = remaining2.first()
-        print(floater)
+    def findCompressed(locations: RDD[Location]) : ArrayBuffer[Location] = {
+        var y = 0
+        var compressed = ArrayBuffer[Location]()
 
-        val locationMap = locations.map(Location => Location)
-        // remove first point because it is the float
-        locations.map(Location => findDistance(anchor, Location, floater))
+        compressed += locations.first()
+        var remaining = locations.mapPartitionsWithIndex{
+            case (index, iterator) => if(index==0) iterator.drop(1) else iterator
+        }
+        while (y < locations.count()) {
+            val anchor = compressed.last
+            // remove everything until new anchor
+
+            var i = 0
+            while(i < remaining.count()-1) {
+                val points = remaining.take(2+i)
+                println("Over here")
+                points.foreach(Location => println(Location))
+                val result = findNewAnchor(anchor,points)
+                println("new anchor index")
+                println(result._1)
+                if(result._2 != -1) {
+                    // add new anchor to compressed list
+                    println("add this point")
+                    println(result._1)
+                    compressed += result._1
+                    i = result._2
+                    i = remaining.count().toInt
+                } else {
+                    i = i+1
+                }
+            }
+            y = y + i
+        }
+        compressed
     }
 
     /** Given 2 points, find the gradient of the line formed */
@@ -67,12 +90,26 @@ object OpeningWindow {
         intercept
     }
 
-    def findDistance(anchor: Location, point: Location, floater: Location) : Double = {
+    def findNewAnchor(anchor: Location, points: Array[Location]) : (Location, Int) = {
+        val threshold = 5.0*Math.pow(10,-8)
+        val floater = points.last
         val grad = findGradient(anchor, floater)
         val intercept = findIntercept(anchor, grad)
-        val distance = Math.abs(grad*point.longitude + (-1)*point.latitude + intercept) / Math.sqrt((grad*grad) + (-1)*(-1))
-        println(distance)
-        distance
+        var i =0
+
+        while(i < points.length) {
+            val distance = Math.abs(grad*points(i).longitude + (-1)*points(i).latitude + intercept) /
+                Math.sqrt((grad*grad) + (-1)*(-1))
+            if(distance > threshold) {
+                println("HERE IS THE DISTANCE")
+                println(distance)
+                println(threshold)
+                return (points(i),i)
+            }
+            i = i+1
+        }
+        (anchor,-1) // anchor remains the same as all points are within expected threshold
+
     }
 
 
